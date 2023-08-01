9832889c25c003c015830c8c6e1a6e35d0aa8502
Anurag Pal <apal895@gmail.com>
2023-08-02 00:41:16 +0530

    added hero section and navbar

---
+++ src/utils/constants.ts
@@ -0,0 +1,61 @@
+export const NAV_ITEMS = [
+  {
+    title: "Home",
+    link: "/",
+  },
+  {
+    title: "About Us",
+    link: "/about-us",
+  },
+  {
+    title: "Academics",
+    links: [
+      {
+        title: "Timetable",
+        link: "/timetable",
+      },
+      {
+        title: "Subjects",
+        link: "/subjects",
+      },
+      {
+        title: "Teachers",
+        link: "/teachers",
+      },
+    ],
+  },
+  {
+    title: "Admissions",
+    links: [
+      {
+        title: "Admission Process",
+        link: "/admission-process",
+      },
+      {
+        title: "Fee Structure",
+        link: "/fee-structure",
+      },
+    ],
+  },
+  {
+    title: "Hostel",
+    links: [
+      {
+        title: "Hostel Rules",
+        link: "/hostel-rules",
+      },
+      {
+        title: "Hostel Facilities",
+        link: "/hostel-facilities",
+      },
+    ],
+  },
+  {
+    title: "Contact Us",
+    link: "/contact-us",
+  },
+  {
+    title: "Fresher's Corner",
+    link: "/freshers-corner",
+  },
+];
